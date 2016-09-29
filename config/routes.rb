7fdf2abad408593f5926846f67c55d8b6d573874
Guilherme Souza <guii_souza@live.com>
2016-09-29 01:43:44 -0300

    Change Font,remove finish navbar, add new route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   mount Spree::Core::Engine, at: '/shop'
   get '/shop/login' => 'spree/user_sessions#new'
+  get '/shop/signup' => 'new_spree_user_registration_path'
   get 'creatives/index'
   root 'creatives#index'
 end
